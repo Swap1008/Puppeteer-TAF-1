fb0d39427c6f05b1afc34c6dbbbe8bbe87ad2655
Swapnil Naidu <swapnilnaidu1008@gmail.com>
2020-01-27 19:23:13 +0530

    Utils Module added

--- tests/FirstExample.js
+++ tests/FirstExample.js
@@ -1,56 +1,46 @@
-const puppeteer=require('puppeteer');
-const expect=require('chai').expect;
-const config=require('../lib/config');
-const helpers=require('../lib/helpers');
-
+const puppeteer = require("puppeteer");
+const expect = require("chai").expect;
+const config = require("../lib/config");
+const helpers = require("../lib/helpers");
 
 // Test Suite
-describe('FirstEx Test', () => {
-    let browser;
-    let page;
-    before(async()=>{
-        browser=await puppeteer.launch({
-            headless:config.isHeadless,
-            slowMo:config.slowMo,
-            timeout:config.launchTimeout
-        });
-        page=await browser.newPage();
-        await page.setDefaultTimeout(config.waitingTimeout);
-        await page.setViewport({
-            height:config.viewPortHeight,
-            width:config.viewPortWidth
-        });
+describe("FirstEx Test", () => {
+  let browser;
+  let page;
+  before(async () => {
+    browser = await puppeteer.launch({
+      headless: config.isHeadless,
+      slowMo: config.slowMo,
+      timeout: config.launchTimeout
     });
-    after(async()=>{
-        await  browser.close();
+    page = await browser.newPage();
+    await page.setDefaultTimeout(config.waitingTimeout);
+    await page.setViewport({
+      height: config.viewPortHeight,
+      width: config.viewPortWidth
     });
-    it('should login as Admin',async()=>{
-        await page.goto(config.baseUrl+'/admin');
-        await page.type('[name="email"]',"admin@phptravels.com");
-        await page.type('[name="password"]','demoadmin');
-        // await page.click('[type="submit"]');
-        await page.click('[type="submit"]');
+  });
+  after(async () => {
+    await browser.close();
+  });
+  it("should login as Admin", async () => {
+    await page.goto(config.baseUrl + "/admin");
+    await page.type('[name="email"]', "admin@phptravels.com");
+    await page.type('[name="password"]', "demoadmin");
+    await page.click('[type="submit"]');
 
-        await page.waitForNavigation();
-        let userRole=await page.$eval('h5', element => element.textContent);
-        
-        
-        expect(userRole).to.equal('AdministratorConsole');
-    });
-    it('should login as user',async()=>{
-        // await page.goto(config.baseUrl+'/login');
-        helpers.loadUrl(page,config.baseUrl+'/login');
-        await page.waitForNavigation();
-        await page.type('[name="username"]','user@phptravels.com');
-        await page.type('[name="password"]','demouser');
-        await page.click('[type="submit"]');
-        await page.waitForNavigation();
-        // await helpers.waitForText(page,'h3','Demo User');
-        // await page.waitForSelector('.dropdown dropdown-login dropdown-tab show');
-        // await helpers.shouldExist(page,'.bx-user');
-        await helpers.click(page,'.bx-user');
-        
-        // await helpers.pressKey(page,'Enter');
-        // await page.$eval('.dropdown dropdown-login dropdown-tab',element=> element.click);
-    });
-})
+    await page.waitForNavigation();
+    let userRole = await page.$eval("h5", element => element.textContent);
+
+    expect(userRole).to.equal("AdministratorConsole");
+  });
+  it("should login as user", async () => {
+    helpers.loadUrl(page, config.baseUrl + "/login");
+    await page.waitForNavigation();
+    await page.type('[name="username"]', "user@phptravels.com");
+    await page.type('[name="password"]', "demouser");
+    await page.click('[type="submit"]');
+    await page.waitForNavigation();
+    await helpers.click(page, ".bx-user");
+  });
+});
---
+++ tests/dataTest.js
@@ -0,0 +1,6 @@
+const utils=require('../lib/utils');
+
+console.log(utils.generateId());
+console.log(utils.generateEmail());
+
+console.log(utils.generateNumbers());
